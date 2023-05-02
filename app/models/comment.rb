ac316998962e68b6e90d77c40653572d31524c68
Teklay <teklay@gmail.com>
2023-05-02 15:25:51 +0300

    Fix linter issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id, class_name: 'User'
-    belongs_to :post, foreign_key: :post_id, class_name: 'Post'
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  belongs_to :post, foreign_key: :post_id, class_name: 'Post'
 
-    def update_comment_counter
-        post.increment!(:comments_counter)
-    end
-    
-    private :update_comment_counter
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
+
+  private :update_comment_counter
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id, class_name: 'User'
-    has_many :comments, foreign_key: :post_id, class_name: 'Comment'
-    has_many :likes, foreign_key: :post_id, class_name: 'Like'
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  has_many :comments, foreign_key: :post_id, class_name: 'Comment'
+  has_many :likes, foreign_key: :post_id, class_name: 'Like'
 
-    def update_post_counter
-        author.update(posts_counter: author.posts.count)
-    end
-    
-    def most_recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
