6274f01fb6c64a6b0aef03178bd0addd001f1b96
Tekhlay <teklaybirhane12@yahoo.com>
2023-05-10 10:28:02 +0300

    Add new method and change redirect path

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  
   def create
     @comment = current_user.comments.new(comment_params)
     @post = Post.find(params[:post_id])
     @comment.post_id = @post.id
-    @comment.save
-    flash[:success] = 'Commented!'
-    redirect_to "/users/#{current_user.id}/posts"
+    if @comment.save
+      flash[:success] = 'Commented!'
+      redirect_to posts_path(@comment.post)
+    else
+      @article = Post.find(params[:post_id])
+      redirect_to posts_path(@post)
+    end
   end
 
   private
