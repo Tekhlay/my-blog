186290754df79d1b312c42459fbd34d6015ce4b1
Tekhlay <teklaybirhane12@yahoo.com>
2023-05-16 17:51:49 +0300

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find_by(id: params[:user_id])
+    #@user = User.includes(:posts, :comments).find(params[:user_id])
     @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 2)
   end
 
   def show
     @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
+    @post = @user.posts.find(params[:id])
+    @comment = Comment.new
+    @like = Like.new
   end
 
   def new
@@ -14,14 +17,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.new(post_params)
-    if @post.save
-      flash[:success] = 'Post created!'
-      redirect_to posts_path(@post)
-    else
-      flash[:danger] = 'Post not created!'
-      render :new, status: :unprocessable_entity
-    end
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+    return unless @post.save
+    redirect_to user_posts_path
   end
 
   private
