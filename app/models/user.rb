f92d3e6741c91c34403d71fee05d417db3ed8356
Tekhlay <teklaybirhane12@yahoo.com>
2023-05-16 17:55:04 +0300

    Add post counter method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def posts_counter
+    posts.count
+  end
+  
   def most_recent_posts
     posts.last(3)
   end
